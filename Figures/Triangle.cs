90e15f70644a19321b1256a54213f737f6991d76
UX32L\Андрей <DronLee@list.ru>
2018-10-10 17:14:57 +0300

    Доработал класс треугольника. 1. Успростил проверку на стороны больше нуля. 2. Добавил документацию.

--- Figures/Triangle.cs
+++ Figures/Triangle.cs
@@ -3,19 +3,35 @@ using System.Linq;
 
 namespace Figures
 {
+    /// <summary>
+    /// Треугольник.
+    /// </summary>
     public class Triangle : IFigure
     {
+        /// <summary>
+        /// Стороны треугольника, отсортированные по размеру.
+        /// </summary>
         private readonly double[] _sortedSides;
 
+        /// <summary>
+        /// Конструкутор.
+        /// </summary>
+        /// <param name="side1">Сторона 1.</param>
+        /// <param name="side2">Сторона 2.</param>
+        /// <param name="side3">Сторона 3.</param>
         public Triangle(double side1, double side2, double side3)
         {
             _sortedSides = new double[] { side1, side2, side3 }.OrderBy(s => s).ToArray();
-            if (_sortedSides.Any(s => s <= 0))
+            if (_sortedSides[0] <= 0)
                 throw new Exception("Все стороны должны быть больше нуля.");
             if (_sortedSides[2] >= _sortedSides[0] + _sortedSides[1])
                 throw new Exception("Ни одна из сторон не должна превышать сумму двух других.");
         }
 
+        /// <summary>
+        /// Вычисление площади треугольника.
+        /// </summary>
+        /// <returns>Площадь треугольника.</returns>
         public double Square()
         {
             double halfMeter = _sortedSides.Sum(s => s) / 2;
@@ -23,6 +39,10 @@ namespace Figures
                 _sortedSides.Select(s => halfMeter - s).Aggregate((p, x) => p *= x), 0.5);
         }
 
+        /// <summary>
+        /// Проверка, является ли треугольник прямоугольным.
+        /// </summary>
+        /// <returns>True - треугольник прямоугольный.</returns>
         public bool IsRectangular()
         {
             return Math.Pow(_sortedSides[2], 2) ==
